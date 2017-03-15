4fd2df93b576f43ce89d62fb9ad1743c947ae064
Antonio Martino <ant.mar94@gmail.com>
2017-03-15 13:01:24 +0100

    aggiunto forgot password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,19 @@ Rails.application.configure do
   config.cache_classes = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "martemmuccio@gmail.com",
+    password: "lucamemmacoglionazzo"
+  }
+
 
 
   # Do not eager load code on boot.
