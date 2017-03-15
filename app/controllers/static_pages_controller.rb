9c120d45382cfd1c66863a11e25e622a46645855
Antonio Martino <ant.mar94@gmail.com>
2017-03-15 10:54:12 +0100

    aggiunte funzioni

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  helper_method :get_spots
   helper_method :get_reviews
 
   def home
@@ -7,19 +8,19 @@ class StaticPagesController < ApplicationController
   def result
   end
 
-private
-  def get_reviews(address)
+  def get_spots(address)
     s=""
     a=Geokit::Geocoders::GoogleGeocoder.geocode(address)
     lat=(a.ll).split(",")[0]
     lng=(a.ll).split(",")[1]
     @client = GooglePlaces::Client.new("AIzaSyDseOM0g-hw8x_uG1EYJOFQ4uMMR8U57KA")
     @spots = @client.spots(lat, lng)
-    for elem in @spots
-      if elem.rating.to_s!=""
-        s=s+elem.name.to_s+", rated: "+elem.rating.to_s+""
-      end
-    end
-    return s
+    return @spots
+  end
+
+  def get_reviews(spot)
+    @client = GooglePlaces::Client.new("AIzaSyDseOM0g-hw8x_uG1EYJOFQ4uMMR8U57KA")
+    reviews = @client.spot(spot.place_id).reviews
+    return reviews
   end
 end
